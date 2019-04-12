ecd39af57e28337f50ae4acc4e64697209dde7b3
matheus.leal <matheus.leal@corp.globo.com>
2019-04-12 07:54:41 -0300

    Update bet system

--- BetOnOneAnimal/BetOnOneAnimal.sol
+++ BetOnOneAnimal/BetOnOneAnimal.sol
@@ -1,17 +1,36 @@
 pragma solidity >=0.4.22 <0.6.0;
 
 contract BetOnOneAnimal {
+  address owner;
+
   struct Bet {
-    address sender;
+    address payable sender;
     uint animal;
     bool voted;
   }
 
   mapping(address => Bet) bets;
+  Bet[] bets;
+  address payable[] winningPlayers;
   string[] animals = ["Avestruz", "Águia", "Burro","Borboleta","Cachorro","Cabra","Carneiro","Camelo","Cobra","Coelho","Cavalo","Elefante","Galo","Gato","Jacaré","Leão","Macaco","Porco","Pavão","Peru","Touro","Tigre","Urso","Veado","Vaca"];
   uint numBets = 0;
-  uint randomNum = 26;
+  uint WinningNumber = 26;
 
+  constructor() public {
+    owner = msg.sender;
+  }
+
+  modifier onlyOwner() {
+      require (msg.sender ==  owner, "Only the contract owner can execute this");
+      _;
+  }
+
+  modifier minCost(uint min) {
+    require(msg.value >= min, "Minumum value of 1000 to bet");
+    _;
+  }
+
+  event sentPrize(uint totalPrize,uint idividualPrize);
   event changeSent(address player, uint change);
 
   function vote(uint number) public payable minCost(1000){
@@ -19,6 +38,7 @@ contract BetOnOneAnimal {
     if (sender.voted || number > 26) return;
     sender.voted = true;
     sender.animal = number - 1;
+    bets.push(sender);
     numBets++;
 
     if(msg.value > 1000){
@@ -28,22 +48,45 @@ contract BetOnOneAnimal {
     }
   }
 
-  modifier minCost(uint min) {
-    require(msg.value >= min, "Minumum value of 1000 to bet");
-    _;
-  }
-
   function getBet() public view returns(string memory _animalSelected){
     Bet storage sender = bets[msg.sender];
     return animals[sender.animal];
   }
 
-  function setWinningAnimal() public returns(uint _animalSelected){
+  function setWinningAnimal() public onlyOwner() returns(string memory _animalSelected){
     require(numBets > 100, "Minumum quantity of 100 bets");
-    if(randomNum == 26){
-      randomNum = random(block.timestamp);
+    if(WinningNumber == 26){
+      WinningNumber = random(block.timestamp);
     }
-    return randomNum;
+    return animals[WinningNumber];
+  }
+
+  function sendMoneyToWinners() public onlyOwner() returns(uint _LastNumSelected) {
+    require(numBets > 100, "Minumum quantity of 100 bets");
+    for(uint p = 0; p < bets.length; p++){
+      address payable player = bets[p].sender;
+      if(bets[p].animal == WinningNumber){
+        winningPlayers.push(player);
+      }
+    }
+
+    uint totalPrize = address(this).balance;
+
+    if(winningPlayers.length > 0){
+      uint idividualPrize = totalPrize / winningPlayers.length;
+
+      for(uint p = 0; p < winningPlayers.length; p++){
+        winningPlayers[p].transfer(idividualPrize);
+      }
+      emit sentPrize(totalPrize,idividualPrize);
+    }
+
+    return WinningNumber;
+  }
+
+  function getWinners() public view returns(address payable[] memory _winningPlayers) {
+    require(numBets > 100, "Minumum quantity of 100 bets");
+    return winningPlayers;
   }
 
   function random(uint seed) private view returns (uint) {
